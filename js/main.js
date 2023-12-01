6d1d8afc3c5afcba89db04fda678891beae93170
Carlos13157 <105325490+Carlos13157@users.noreply.github.com>
2023-12-01 18:53:08 -0500

    six commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 import {VRButton} from '/libraries/VRButton.js';
+//import { XRControllerModelFactory } from '/libraries/XRControllerModelFactory.js';
+//import { XRHandModelFactory } from '/libraries/XRHandModelFactory.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -9,6 +11,32 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.xr.enabled = true;
 document.body.appendChild(renderer.domElement);
 document.body.appendChild( VRButton.createButton( renderer ) );
+
+const controller = renderer.xr.getController(0);
+controller.addEventListener('selectstart', onSelectStart);
+controller.addEventListener('selectend', onSelectEnd);
+scene.add(controller);
+
+const reticle = new THREE.Mesh(
+    new THREE.RingGeometry(0.02, 0.04, 32).rotateX(-Math.PI / 2),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+  );
+  reticle.matrixAutoUpdate = false;
+  reticle.visible = false;
+  scene.add(reticle);
+  
+
+function onSelectStart(event) {
+    // Puedes realizar acciones cuando el botón de selección es presionado
+  }
+  
+  function onSelectEnd(event) {
+    // Puedes realizar acciones cuando el botón de selección es liberado
+  }
+
+
+
+
 
 scene.background = new THREE.CubeTextureLoader()
     .setPath('Materials/')
@@ -45,29 +73,50 @@ function crearCubos() {
     }
 }
 
-// Objeto estático
-const staticGeometry = new THREE.SphereGeometry(0.005, 32, 32);
-const staticMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff }); // Cambiado el color para diferenciarlo
-const staticSphere = new THREE.Mesh(staticGeometry, staticMaterial);
-scene.add(staticSphere);
 
-
-//camera.position.z = -5;
 camera.position.set(0, 0, -3); // Ajusta la posición inicial de la cámara
 
 
 const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
 
+function handleController(controller) {
+    const tempMatrix = new THREE.Matrix4();
+    const position = new THREE.Vector3();
+    const quaternion = new THREE.Quaternion();
+    const scale = new THREE.Vector3();
+
+  
+    if (controller.hand) {
+      const inputSource = renderer.xr.getController(controller.hand === 'right' ? 1 : 0).inputSource;
+  
+      if (inputSource) {
+        inputSource.matrixWorld.decompose(position, quaternion, scale);
+        reticle.position.setFromMatrixPosition(inputSource.matrixWorld);
+        reticle.quaternion.setFromRotationMatrix(tempMatrix.extractRotation(inputSource.matrixWorld));
+        reticle.scale.setFromMatrixScale(inputSource.matrixWorld);
+      }
+    }
+  
+    // Actualizar la escena
+    renderer.render(scene, camera);
+  }
+
 
 function animate() {
     requestAnimationFrame(animate);
+
+    ////////////////////////////////////////////////////////
+
+    renderer.setAnimationLoop(() => {
+        handleController(controller);
+      });
 
     /////////////////////////////////////////////////////////////////
 
     raycaster.setFromCamera( pointer, camera );
 
-    const intersects = raycaster.intersectObjects(scene.children.filter(obj => obj !== staticSphere));
+    const intersects = raycaster.intersectObjects(scene.children.filter(obj => obj !== reticle));
 
     for ( let i = 0; i < intersects.length; i ++ ) {
 
@@ -80,19 +129,6 @@ function animate() {
         cubo.rotation.x += 0.01;
         cubo.rotation.y += 0.01;
     });
-
-    // Asigna la posición de la cámara al objeto estático
-    staticSphere.position.copy(camera.position);
-
-    // Obtiene la dirección de la cámara en el espacio del mundo y la usa para establecer la orientación del staticSphere
-    const cameraDirection = new THREE.Vector3();
-    camera.getWorldDirection(cameraDirection);
-    staticSphere.lookAt(staticSphere.position.clone().add(cameraDirection));
-
-    // Ajusta la posición del staticSphere para que esté siempre en frente de la cámara
-    const distance = 0.15; // Ajusta la distancia según sea necesario
-    const newPosition = camera.position.clone().add(cameraDirection.multiplyScalar(distance));
-    staticSphere.position.copy(newPosition);
 
     window.addEventListener( 'pointermove', onPointerMove );
 }
