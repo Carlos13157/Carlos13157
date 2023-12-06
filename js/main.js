67501c275c10801ca04ce40b8b2395c28184300a
Carlos13157 <105325490+Carlos13157@users.noreply.github.com>
2023-12-05 22:02:57 -0500

    restored to original version

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
-import { VRButton } from '/libraries/VRButton.js';
+import {VRButton} from '/libraries/VRButton.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.xr.enabled = true;
 document.body.appendChild(renderer.domElement);
-document.body.appendChild(VRButton.createButton(renderer));
+document.body.appendChild( VRButton.createButton( renderer ) );
 
 scene.background = new THREE.CubeTextureLoader()
     .setPath('Materials/')
@@ -20,20 +21,12 @@ scene.background = new THREE.CubeTextureLoader()
         'rainbow_bk.png'
     ]);
 
-const BackgroundGeometry = new THREE.SphereGeometry(100, 32, 16);
+    const BackgroundGeometry = new THREE.SphereGeometry( 100, 32, 16 );
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
-
-// Agregamos el objeto marker
-const marker = new THREE.Mesh(
-    new THREE.CircleGeometry(0.05, 32).rotateX(-Math.PI / 2),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
-marker.visible = false;
-scene.add(marker);
 
 function crearCubos() {
     var cantidadDeCubos = 100;
@@ -52,51 +45,50 @@ function crearCubos() {
     }
 }
 
-// Ajusta la posición inicial de la cámara
-camera.position.set(0, 0, -3);
+
+
+//camera.position.z = -5;
+camera.position.set(0, 0, -3); // Ajusta la posición inicial de la cámara
+
 
 const raycaster = new THREE.Raycaster();
-const controller = renderer.xr.getController(0); // Solo se utiliza un controlador para simplificar
+const pointer = new THREE.Vector2();
 
-// Añade el controlador a la escena
-scene.add(controller);
-
-// Escucha los eventos del controlador
-controller.addEventListener('selectstart', onSelectStart);
-controller.addEventListener('selectend', onSelectEnd);
-
-function onSelectStart(event) {
-    const intersections = getIntersections();
-
-    if (intersections.length > 0) {
-        const point = intersections[0].point;
-        marker.position.copy(point);
-        marker.visible = true;
-    }
-}
-
-function onSelectEnd() {
-    marker.visible = false;
-}
-
-function getIntersections() {
-    raycaster.setFromController(controller);
-
-    return raycaster.intersectObjects(scene.children);
-}
 
 function animate() {
     requestAnimationFrame(animate);
 
-    // Rotación de los cubos
+    /////////////////////////////////////////////////////////////////
+
+    raycaster.setFromCamera( pointer, camera );
+
+    const intersects = raycaster.intersectObjects(scene.children);
+
+    for ( let i = 0; i < intersects.length; i ++ ) {
+
+        intersects[ i ].object.material.color.set( 0xff0000 );
+    
+    }
+    ////////////////////////////////////////////////////////////////
+     // Rotación de los cubos
     scene.children.forEach(function (cubo) {
         cubo.rotation.x += 0.01;
         cubo.rotation.y += 0.01;
     });
 
-    renderer.render(scene, camera);
+
+
+    window.addEventListener( 'pointermove', onPointerMove );
 }
 
-renderer.setAnimationLoop(animate);
+function onPointerMove( event ) {
+	pointer.x = 0;
+	pointer.y = 0;
+}
+
+renderer.setAnimationLoop( function () {
+	renderer.render( scene, camera );
+} );
 
 crearCubos();
+animate();
