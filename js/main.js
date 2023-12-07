3a3f24a9be8f57f1fd3e8c270e12ac4bb9a883dc
Carlos13157 <105325490+Carlos13157@users.noreply.github.com>
2023-12-07 00:18:35 -0500

    Fixing VR Problems #4

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 import * as THREE from './three.module.js';
-import {VRButton} from '/libraries/VRButton.js';
-import {OrbitControls} from './orbitcontrols.js';
+import { VRButton } from '/libraries/VRButton.js';
+import { OrbitControls } from './orbitcontrols.js';
 
 const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.xr.enabled = true;
-  document.body.appendChild(renderer.domElement);
-  document.body.appendChild( VRButton.createButton( renderer ) );
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.xr.enabled = true;
+document.body.appendChild(renderer.domElement);
+document.body.appendChild(VRButton.createButton(renderer));
 
-  const cameraMin = 0.0001;
+const cameraMin = 0.0001;
 
-  const aspect = window.innerWidth / window.innerHeight;
-  const camera = new THREE.PerspectiveCamera(75, aspect, cameraMin, 1000);
-  const controls = new OrbitControls(camera, renderer.domElement);
+const aspect = window.innerWidth / window.innerHeight;
+const camera = new THREE.PerspectiveCamera(75, aspect, cameraMin, 1000);
+const controls = new OrbitControls(camera, renderer.domElement);
 // Bloquea todas las funciones del OrbitControls
 controls.enableRotate = false;
 controls.enablePan = false;
@@ -20,157 +20,142 @@ controls.enableZoom = false;
 controls.enableDamping = false;
 controls.enableKeys = false;
 
-  const scene = new THREE.Scene();
+// Añade el siguiente bloque para manejar el modo VR
+renderer.xr.addEventListener('sessionstart', function () {
+  controls.enabled = false;
+});
 
-  camera.position.z = 5;
+renderer.xr.addEventListener('sessionend', function () {
+  controls.enabled = true;
+});
 
-  scene.add(camera);
+const scene = new THREE.Scene();
 
-  scene.background = new THREE.CubeTextureLoader()
-    .setPath('Materials/')
-    .load([
-        'rainbow_lf.png',
-        'rainbow_rt.png',
-        'rainbow_up.png',
-        'rainbow_dn.png',
-        'rainbow_ft.png',
-        'rainbow_bk.png'
-    ]);
+camera.position.z = 5;
 
-    const BackgroundGeometry = new THREE.SphereGeometry( 100, 32, 16 );
+scene.add(camera);
 
-	const light = new THREE.PointLight( 0xffffff, 1, 100 );
-	scene.add( light );
-	const Ambientlight = new THREE.AmbientLight( 0x404040 ); // soft white light
-	scene.add( Ambientlight );
+scene.background = new THREE.CubeTextureLoader()
+  .setPath('Materials/')
+  .load([
+    'rainbow_lf.png',
+    'rainbow_rt.png',
+    'rainbow_up.png',
+    'rainbow_dn.png',
+    'rainbow_ft.png',
+    'rainbow_bk.png'
+  ]);
 
-    const selectable = [];
-	
-	function crearCubos() {
-		const cantidadDeCubos = 100;
-	
-		for (var i = 0; i < cantidadDeCubos; i++) {
-			const geometry = new THREE.BoxGeometry();
-			const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
-			const Cubo = new THREE.Mesh(geometry, material);
-	
-			// Asigna posiciones aleatorias a los 
-			Cubo.position.x = (Math.random() - 0.5) * 10;
-			Cubo.position.y = (Math.random() - 0.5) * 10;
-			Cubo.position.z = (Math.random() - 0.5) * 10;
+const BackgroundGeometry = new THREE.SphereGeometry(100, 32, 16);
 
-            scene.add(Cubo);
+const light = new THREE.PointLight(0xffffff, 1, 100);
+scene.add(light);
+const Ambientlight = new THREE.AmbientLight(0x404040); // soft white light
+scene.add(Ambientlight);
 
-			selectable.push({
-				selected: false,
-				object: Cubo,
-				action() {
-				  console.log("Cubo selected");
-				},
-			  });
-		}
-	}
-	
-  const cursorSize = 1;
-  const cursorThickness = 1.5;
-  const cursorGeometry = new THREE.RingBufferGeometry(
-    cursorSize * cameraMin,
-    cursorSize * cameraMin * cursorThickness,
-    32,
-    0,
-    Math.PI * 0.5,
-    Math.PI * 2
-  );
-  const cursorMaterial = new THREE.MeshBasicMaterial({ color: "white" });
-  const cursor = new THREE.Mesh(cursorGeometry, cursorMaterial);
+const selectable = [];
 
-  cursor.position.z = -cameraMin * 50;
+function crearCubos() {
+  const cantidadDeCubos = 100;
 
-  camera.add(cursor);
+  for (var i = 0; i < cantidadDeCubos; i++) {
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+    const Cubo = new THREE.Mesh(geometry, material);
 
-  const raycaster = new THREE.Raycaster();
-  const pointer = new THREE.Vector2();
+    // Asigna posiciones aleatorias a los cubos
+    Cubo.position.x = (Math.random() - 0.5) * 10;
+    Cubo.position.y = (Math.random() - 0.5) * 10;
+    Cubo.position.z = (Math.random() - 0.5) * 10;
 
-  const raycaster2 = new THREE.Raycaster();
+    scene.add(Cubo);
 
-  let firstRun = true;
-  function animate() {
-    requestAnimationFrame(animate);
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    controls.update();
-
-	/////////////////////////////
-	  /////////////////////////////////////////////////////////////////
-
-	  raycaster.setFromCamera( pointer, camera );
-
-	  const intersects = raycaster.intersectObjects(scene.children);
-  
-	  for (let i = 0; i < intersects.length; i++) {
-		const intersectedObject = intersects[i].object;
-	  
-		// Cambiar color solo si el objeto intersectado es un cubo seleccionable
-		if (intersectedObject.userData.isSelectable) {
-		  intersectedObject.material.color.set(0xff0000);
-		}
-	  }
-	  ////////////////////////////////////////////////////////////////
-	   // Rotación de los cubos
-	  scene.children.forEach(function (cubo) {
-		  cubo.rotation.x += 0.01;
-		  cubo.rotation.y += 0.01;
-	  });
-	/////////////////////////////
-
-    if (!firstRun) {
-      for (let i = 0, length = selectable.length; i < length; i++) {
-        const camPosition = camera.position.clone();
-        const objectPosition = selectable[i].object.position.clone();
-        raycaster2.set(camPosition, camera.getWorldDirection(objectPosition));
-
-        const intersects2 = raycaster2.intersectObject(selectable[i].object);
-
-        const selected = intersects2.length > 0;
-
-        cursor.material.color.set(selected ? new THREE.Color("crimson") : new THREE.Color("white"));
-
-		      // Cambiar color del objeto solo cuando está seleccionado
-			  if (selected) {
-				selectable[i].object.material.color.set(0xff0000);
-			  } else {
-				selectable[i].object.material.color.set(0x00ff00);
-			  }
-		
-			  if (selected && !selectable[i].selected) {
-				selectable[i].action();
-			  }
-			  selectable[i].selected = selected;
-			}
-		  }
-
-	 // Cambia el color del cursor aquí si es necesario
-	 cursor.material.color.set(selectable.some(obj => obj.selected) ? new THREE.Color("crimson") : new THREE.Color("white"));
-
-
-
-	window.addEventListener( 'pointermove', onPointerMove );
-    firstRun = false;
-  };
-
-  function onPointerMove( event ) {
-	pointer.x = 0;
-	pointer.y = 0;
+    selectable.push({
+      selected: false,
+      object: Cubo,
+      action() {
+        console.log("Cubo selected");
+      },
+    });
+  }
 }
 
-renderer.setAnimationLoop( function () {
+const cursorSize = 1;
+const cursorThickness = 1.5;
+const cursorGeometry = new THREE.RingBufferGeometry(
+  cursorSize * cameraMin,
+  cursorSize * cameraMin * cursorThickness,
+  32,
+  0,
+  Math.PI * 0.5,
+  Math.PI * 2
+);
+const cursorMaterial = new THREE.MeshBasicMaterial({ color: "white" });
+const cursor = new THREE.Mesh(cursorGeometry, cursorMaterial);
 
-	renderer.render( scene, camera );
+cursor.position.z = -cameraMin * 50;
 
-} );
+camera.add(cursor);
 
-  crearCubos();
-  animate();
+const raycaster2 = new THREE.Raycaster();
+
+let firstRun = true;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  controls.update();
+
+  // Rotación de los cubos
+  scene.children.forEach(function (cubo) {
+    cubo.rotation.x += 0.01;
+    cubo.rotation.y += 0.01;
+  });
+
+  if (!firstRun) {
+    for (let i = 0, length = selectable.length; i < length; i++) {
+      const camPosition = camera.position.clone();
+      const objectPosition = selectable[i].object.position.clone();
+      raycaster2.set(camPosition, camera.getWorldDirection(objectPosition));
+
+      const intersects2 = raycaster2.intersectObject(selectable[i].object);
+
+      const selected = intersects2.length > 0;
+
+      cursor.material.color.set(selected ? new THREE.Color("crimson") : new THREE.Color("white"));
+
+      // Cambiar color del objeto solo cuando está seleccionado
+      if (selected) {
+        selectable[i].object.material.color.set(0xff0000);
+      } else {
+        selectable[i].object.material.color.set(0x00ff00);
+      }
+
+      if (selected && !selectable[i].selected) {
+        selectable[i].action();
+      }
+      selectable[i].selected = selected;
+    }
+  }
+
+  // Cambia el color del cursor aquí si es necesario
+  cursor.material.color.set(selectable.some(obj => obj.selected) ? new THREE.Color("crimson") : new THREE.Color("white"));
+
+  window.addEventListener('pointermove', onPointerMove);
+  firstRun = false;
+}
+
+function onPointerMove(event) {
+  // Puedes agregar lógica adicional aquí si es necesario
+}
+
+renderer.setAnimationLoop(function () {
+  renderer.render(scene, camera);
+});
+
+crearCubos();
+animate();
